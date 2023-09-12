b6fe1831fab5b847d3737b9d8402bd015e16e514
Dludora <1115059192@qq.com>
2023-09-12 22:42:03 +0800

    start

--- Container/views.py
+++ Container/views.py
@@ -5,15 +5,34 @@ from django.shortcuts import render
 import docker
 from django.views.decorators.csrf import csrf_exempt
 
+client = docker.from_env()
+
 
 def ctn_list():
-    client = docker.APIClient(base_url='tcp://10.251.255.171:2375')
+    # client = docker.APIClient(base_url='tcp://10.251.255.171:2375')
     containers = client.containers.list(all=True)
     res = []
+    print(containers[0].__dict__)
+
     for container in containers:
-        res.append(container)
+        # image = client.images.get(container.attrs['Image'])
+        res.append({
+            'id': container.id,
+            'name': container.name,
+            'image_id': container.attrs['Image'],
+            'status': container.attrs['State']['Status'],
+            'ports': container.attrs['NetworkSettings']['Ports'],
+            'start_at': container.attrs['State']['StartedAt']},
+        )
 
     return res
+
+def ctn_get(container_id):
+    container = client.containers.get(container_id)
+    container_attr = {}
+    container_attr['id'] = container.id
+    
+
 
 
 @csrf_exempt
@@ -22,5 +41,4 @@ def list_containers(request):
 
     res = ctn_list()
     return JsonResponse({'status': 0, 'res': res})
-
 
